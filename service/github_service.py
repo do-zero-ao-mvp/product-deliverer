bad650c700efaa759544037da015c3be7c143dea
Flavio Mendes <flaviojmendes@gmail.com>
2023-03-08 13:31:18 +0000

    adicionar ao repositorio

--- service/github_service.py
+++ service/github_service.py
@@ -9,17 +9,25 @@ async def add_github_permission(user_email: str, product_id: str):
 
     result = []
     organization = g.get_organization("do-zero-ao-mvp")
+    repo = organization.get_repo("ebook")
     team_name = 'zeros'
     # TYPES OF ROLES= ["admin", "direct_member", "billing_manager"], TEAMS is ARRAY
     try:
-        teams = organization.get_teams()
-        for t in teams:
-            if t.name == team_name:
-                team = t
-        result = organization.invite_user(email=user_email,
-                role="direct_member",
-                teams=[team])
+        if repo.get_collaborator(user_email) is None:
+            repo.add_to_collaborators(user_email, permission="read")
+            print(f"Successfully added {user_email} to {repo.name}")
+        else:
+            print(f"{user_email} already has access to {repo.name}")
+
+
+        # teams = organization.get_teams()
+        # for t in teams:
+        #     if t.name == team_name:
+        #         team = t
+        # result = organization.invite_user(email=user_email,
+        #         role="direct_member",
+        #         teams=[team])
     except Exception as error:
         raise error
-    if result is None:
-        print(f"Successfully sent invite")
+    # if result is None:
+    #     print(f"Successfully sent invite")
